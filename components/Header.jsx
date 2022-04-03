cc7c5bd3059e653301651abfe228b021cadda26a
Huynh Ngoc Thien <thien1367526@gmail.com>
2022-04-03 17:24:33 +0700

    Adding navigation and accessibility tweaks

---
+++ components/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { images } from '../public/constants';
+import { useState } from 'react';
+
+const Header = () => {
+   const [open, setOpen] = useState(false);
+
+   return (
+      <header className="primary-header flex">
+         <div className="logo">
+            <Image src={images.logo} layout='fixed' alt="Space logo" />
+         </div>
+
+         <button onClick={() => setOpen(!open)} className='mobile-nav-toggle' aria-controls='primary-nav' style={{ backgroundImage: `url(/assets/shared/icon-${open ? 'close' : 'hamburger'}.svg)` }}><span className="sr-only" aria-expanded='false'>Menu</span></button>
+
+         <nav>
+            <ul id='primary-nav' class={`flex primary-navigation underlined-indicator ${open ? 'slide-in' : ''}`}>
+               <li className='uppercase ff-sans-cond letter-spacing-2 fs-300 active'>
+                  <Link href='/'><a><span aria-hidden='true'>00</span>Home</a></Link>
+               </li>
+               <li className='uppercase ff-sans-cond letter-spacing-2 fs-300'>
+                  <Link href='/destination'><a><span aria-hidden='true'>01</span>Destination</a></Link>
+               </li>
+               <li className='uppercase ff-sans-cond letter-spacing-2 fs-300'>
+                  <Link href='/crew'><a><span aria-hidden='true'>02</span>Crew</a></Link>
+               </li>
+               <li className='uppercase ff-sans-cond letter-spacing-2 fs-300'>
+                  <Link href='/technology'><a><span aria-hidden='true'>03</span>Technology</a></Link>
+               </li>
+            </ul>
+         </nav>
+      </header>
+   )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,12 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { images } from '../public/constants';
+import Header from '../components/Header';
 
 export default function Home() {
    return (
       <div className='home'>
-         <header className="primary-header flex">
-            <div className="logo">
-               <Image src={images.logo} layout='fixed' alt="Space logo" />
-            </div>
+         <a href="#main" className='skip-to-main'>Skip to main</a>
+         <Header />
 
-            {/* <nav>
-               <ul class="flex primary-navigation underlined-indicator">
-                  <li className='uppercase ff-sans-cond letter-spacing-2 fs-300 active'>
-                     <Link href='/'><a><span>00</span>Home</a></Link>
-                  </li>
-                  <li className='uppercase ff-sans-cond letter-spacing-2 fs-300'>
-                     <Link href='/destination'><a><span>01</span>Destination</a></Link>
-                  </li>
-                  <li className='uppercase ff-sans-cond letter-spacing-2 fs-300'>
-                     <Link href='/crew'><a><span>02</span>Crew</a></Link>
-                  </li>
-                  <li className='uppercase ff-sans-cond letter-spacing-2 fs-300'>
-                     <Link href='/technology'><a><span>03</span>Technology</a></Link>
-                  </li>
-               </ul>
-            </nav> */}
-         </header>
-
-         <div className="grid-container grid-container--home">
+         <main id='main' className="grid-container grid-container--home">
             <div>
                <h1 className='text-accent fs-500 ff-sans-cond uppercase letter-spacing-1'>So, you want to travel to<br/><span className='fs-900 text-white ff-serif'>Space</span></h1>
                
@@ -36,7 +14,7 @@ export default function Home() {
             </div>
 
             <div><a href="#" className='large-btn uppercase ff-serif text-dark bg-white grid'>Explore</a></div>
-         </div>
+         </main>
       </div>
    )
 }
